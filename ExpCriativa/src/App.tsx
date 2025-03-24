11f2792b85cce2f54ec5109d70d0a05baf0e9a37
Amanda Queiroz <amandaq.sobral@gmail.com>
2025-03-24 20:41:42 -0300

    Added login and signup page for organizations

--- ExpCriativa/src/App.tsx
+++ ExpCriativa/src/App.tsx
@@ -6,6 +6,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import Login from "./pages/Login";
+import LoginOrg from "./pages/LoginOrg";
 import NotFound from "./pages/NotFound";
 
 // Create a QueryClient instance
@@ -22,6 +23,7 @@ const App = () => {
           <Routes>
             <Route path="/" element={<Index />} />
             <Route path="/login" element={<Login />} />
+            <Route path="/loginOrg" element={<LoginOrg />} />
             {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
             <Route path="*" element={<NotFound />} />
           </Routes>
