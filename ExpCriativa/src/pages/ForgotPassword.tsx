d445607579292c5caae62123167e62733c8c23de
carloshobmeier <carloshobmeier@hotmail.com>
2025-05-11 09:31:25 -0300

    changed placeholder to english

--- ExpCriativa/src/pages/ForgotPassword.tsx
+++ ExpCriativa/src/pages/ForgotPassword.tsx
@@ -79,11 +79,11 @@ const ForgotPassword: React.FC = () => {
                             render={({ field }) => (
                                 <FormItem>
                                     <FormLabel className="text-sm font-medium text-gray-700">
-                                        Registered E-mail:
+                                        Registered e-mail:
                                     </FormLabel>
                                     <Input
                                         {...field}
-                                        placeholder="seu@exemplo.com"
+                                        placeholder="your-email@example.com"
                                         className="mt-1 block w-full rounded-md border-gray-300
                                shadow-sm focus:border-indigo-500
                                focus:ring focus:ring-indigo-200
